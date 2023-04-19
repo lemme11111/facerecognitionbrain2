3536584f65f56b1b1fd835938a50fbd44598692c
lemme11111 <123477941+lemme11111@users.noreply.github.com>
2023-04-19 10:58:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://face-recognition-z6yi.onrender.com/image', {
+          fetch('https://mybackend-uve8.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
